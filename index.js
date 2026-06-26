b316e74be4ec994612ba99aba4bb78840f9fde38
ALOK SINGH <alok7233singh@gmail.com>
2026-06-26 12:01:48 +0530

    alok singh dear

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("hello world");
+const express = require("express")
+
+const app = express();
+
+let students =[
+    {id: 1, name: "alok singh", city: "gkp" },
+    {id: 2, name: "gaurav yadav", city: "sahjnwa"},
+];
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("api is running");
+});
+
+app.get("/students",(req,res)=>{
+    res.json({
+        message:"all students",
+        data:students
+    })
+});
+
+app.post("/students",(req,res)=>{
+    const { id,name,city} = req.body;
+    const newStudent = { id,name,city };
+    students.push(newStudent);
+    res.json({
+        message:"record added",
+        student:newStudent,
+        data:students
+    })
+});
+
+app.listen(3000,()=>{
+    console.log("server started");
+});
